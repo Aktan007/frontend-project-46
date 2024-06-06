eae76156e79277805b4de9242409c25b52d2b094
Aktan <aktan.ibraev05@mail.ru>
2024-06-06 15:01:41 +0200

    creting project

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log("Успешно!");
+import { Command } from 'commander';
+const program = new Command();
+
+program
+//  .name('string-util')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.8.0')
+  .parse(process.argv);
