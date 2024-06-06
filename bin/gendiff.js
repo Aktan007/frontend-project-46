e291d8a31dd20bd26824de95017d581bfe7858c5
Aktan <aktan.ibraev05@mail.ru>
2024-06-06 15:18:49 +0200

    finish second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,8 @@ const program = new Command();
 program
 //  .name('string-util')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.8.0')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format [type]','output format')
+  .arguments('<filepath1>, <filepath2')
+  .helpOption('-h, --help','output usage information')
   .parse(process.argv);
