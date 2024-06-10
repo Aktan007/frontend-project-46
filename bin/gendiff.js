4d4470bf3c665f6959db97b126691a147e7afa3a
Aktan <you@example.com>
2024-06-10 10:14:08 +0300

    pass 3 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import gendiff  from '../src/index.js';
 const program = new Command();
 
 program
@@ -9,5 +10,7 @@ program
   .version('1.0.0', '-V, --version', 'output the version number')
   .option('-f, --format [type]','output format')
   .arguments('<filepath1>, <filepath2')
+  .action((filepath1, filepath2) =>
+    gendiff(filepath1, filepath2))
   .helpOption('-h, --help','output usage information')
   .parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import path from 'node:path'
+import { readFileSync } from 'node:fs'
+
+const gendiff = (filepath1, filepath2) => {
+    const fullFilepath1 = path.resolve(filepath1);
+    const fullFilepath2 = path.resolve(filepath2);
+    const file1 = readFileSync(fullFilepath1)
+    const file2 = readFileSync(fullFilepath2)
+    const fileJson1 = JSON.parse(file1);
+    const fileJson2 = JSON.parse(file2);
+
+    console.log(fileJson1);
+    console.log(fileJson2);
+};
+
+export default gendiff;
